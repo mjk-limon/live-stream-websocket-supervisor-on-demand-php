b34b996cd88f0e966028f670d6d6de198da22f85
mjk-limon <jhmasterlimon11@gmail.com>
2024-12-28 14:49:39 +0600

    Refactor WebSocket handling and update user picture paths

--- app/check_user.php
+++ app/check_user.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once __DIR__ . "/../vendor/autoload.php";
+require_once __DIR__ . "/helpers/supervisor_helpers.php";
 header('Content-Type: application/json');
 
 $username = $_POST['username'];
@@ -7,12 +9,12 @@ $usernames = [
     [
         'id' => 12232,
         'name' => 'জাহিদ লিমন',
-        'pic' => 'https://via.placeholder.com/50',
+        'pic' => 'assets/ppic/12232.jpg',
     ],
     [
         'id' => 12233,
         'name' => 'কনক',
-        'pic' => 'https://via.placeholder.com/50',
+        'pic' => 'assets/ppic/12233.jpg',
     ],
 ];
 
@@ -20,9 +22,9 @@ $found = array_search($username, array_column($usernames, 'id'));
 
 if ($found !== false) {
     http_response_code(200);
+    start_socket_if_not_running();
     exit(json_encode(['exists' => true, 'user' => $usernames[$found]]));
 }
-
 
 http_response_code(404);
 exit(json_encode(['exists' => false]));
--- app/socket.php
+++ app/socket.php
@@ -1,138 +1,75 @@
 <?php
 
-$host = 'localhost';
-$port = '9000';
-$null = NULL;
+require_once __DIR__ . "/helpers/socket_helpers.php";
+set_time_limit(0);
+ob_implicit_flush();
 
-function send_message($msg)
-{
-    global $clients;
-    foreach ($clients as $changed_socket) {
-        @socket_write($changed_socket, $msg, strlen($msg));
-    }
-    return true;
-}
+$address = '0.0.0.0';
+$port = 9000;
 
-function unmask($text)
-{
-    $length = ord($text[1]) & 127;
-    if ($length == 126) {
-        $masks = substr($text, 4, 4);
-        $data = substr($text, 8);
-    } elseif ($length == 127) {
-        $masks = substr($text, 10, 4);
-        $data = substr($text, 14);
-    } else {
-        $masks = substr($text, 2, 4);
-        $data = substr($text, 6);
-    }
-    $text = "";
-    for ($i = 0; $i < strlen($data); ++$i) {
-        $text .= $data[$i] ^ $masks[$i % 4];
-    }
-    return $text;
-}
-
-function mask($text)
-{
-    $b1 = 0x80 | (0x1 & 0x0f);
-    $length = strlen($text);
-
-    if ($length <= 125)
-        $header = pack('CC', $b1, $length);
-    elseif ($length > 125 && $length < 65536)
-        $header = pack('CCn', $b1, 126, $length);
-    elseif ($length >= 65536)
-        $header = pack('CCNN', $b1, 127, $length);
-
-    return $header . $text;
-}
-
-function perform_handshaking($receved_header, $client_conn, $host, $port)
-{
-    $headers = array();
-    $lines = preg_split("/\r\n/", $receved_header);
-
-    
-
-    foreach ($lines as $line) {
-        $line = chop($line);
-        if (preg_match('/\A(\S+): (.*)\z/', $line, $matches)) {
-            $headers[$matches[1]] = $matches[2];
-        }
-    }
-
-    $secKey = $headers['Sec-Websocket-Key'] ?? $headers['Sec-WebSocket-Key'];
-    $secAccept = base64_encode(pack('H*', sha1($secKey . '258EAFA5-E914-47DA-95CA-C5AB0DC85B11')));
-
-    $upgrade  = "HTTP/1.1 101 Web Socket Protocol Handshake\r\n" .
-        "Upgrade: websocket\r\n" .
-        "Connection: Upgrade\r\n" .
-        "WebSocket-Origin: $host\r\n" .
-        "WebSocket-Location: ws://$host:$port/demo/shout.php\r\n" .
-        "Sec-WebSocket-Accept:$secAccept\r\n\r\n";
-
-    socket_write($client_conn, $upgrade, strlen($upgrade));
-}
-
+$clients = [];
+$last_activity = time();
 
 $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
 socket_set_option($socket, SOL_SOCKET, SO_REUSEADDR, 1);
-socket_bind($socket, 0, $port);
+socket_bind($socket, $address, $port);
 socket_listen($socket);
 
-$clients = array($socket);
-
 while (true) {
-    $changed = $clients;
+    $read = array_merge([$socket], $clients);
+    $write = null;
+    $except = null;
 
-    socket_select($changed, $null, $null, 0, 10);
+    $num_changed_sockets = socket_select($read, $write, $except, 1);
 
-    if (in_array($socket, $changed)) {
-        $socket_new = socket_accept($socket);
-        $clients[] = $socket_new;
-
-        $header = socket_read($socket_new, 1024);
-        perform_handshaking($header, $socket_new, $host, $port);
-
-        // socket_getpeername($socket_new, $ip);
-        // $response = mask(json_encode(array('type' => 'system', 'message' => $ip . ' connected')));
-        // send_message($response);
-
-        $found_socket = array_search($socket, $changed);
-        unset($changed[$found_socket]);
+    if ($num_changed_sockets === false) {
+        break;
     }
 
+    if ($num_changed_sockets > 0) {
+        if (in_array($socket, $read)) {
+            $new_socket = socket_accept($socket);
+            $header = socket_read($new_socket, 1024);
+            perform_handshaking($header, $new_socket, $address, $port);
 
-    foreach ($changed as $changed_socket) {
-        while (socket_recv($changed_socket, $buf, 1024, 0) >= 1) {
-            $received_text = unmask($buf);
-            $tst_msg = json_decode($received_text, true);
+            $clients[] = $new_socket;
+            $last_activity = time();
+        }
 
-            if ($tst_msg !== null) {
-                $msgtype = $tst_msg['type'];
-                $user_pic = $tst_msg['pic'];
-                $user_name = $tst_msg['name'];
-                $user_message = $tst_msg['message'];
+        foreach ($read as $changed_socket) {
+            if ($changed_socket !== $socket) {
+                while (socket_recv($changed_socket, $buf, 1024, 0) >= 1) {
+                    $received_text = unmask($buf);
+                    $tst_msg = json_decode($received_text, true);
 
-                //prepare data to be sent to client
-                $response_text = mask(json_encode(array('type' => $msgtype, 'name' => $user_name, 'message' => $user_message, 'pic' => $user_pic)));
-                send_message($response_text);
+                    if ($tst_msg !== null) {
+                        $msgtype = $tst_msg['type'];
+                        $user_pic = $tst_msg['pic'];
+                        $user_name = $tst_msg['name'];
+                        $user_message = $tst_msg['message'];
+
+                        //prepare data to be sent to client
+                        $response_text = mask(json_encode(array('type' => $msgtype, 'name' => $user_name, 'message' => $user_message, 'pic' => $user_pic)));
+                        send_message($response_text);
+                    }
+
+                    $last_activity = time();
+                    break 2;
+                }
+
+                $buf = @socket_read($changed_socket, 1024, PHP_NORMAL_READ);
+                if ($buf === false) {
+                    $found_socket = array_search($changed_socket, $clients);
+                    unset($clients[$found_socket]);
+                }
             }
-
-            break 2;
         }
+    }
 
-        $buf = @socket_read($changed_socket, 1024, PHP_NORMAL_READ);
-        if ($buf === false) {
-            $found_socket = array_search($changed_socket, $clients);
-            unset($clients[$found_socket]);
-
-            // socket_getpeername($changed_socket, $ip);
-            // $response = mask(json_encode(array('type' => 'system', 'message' => $ip . ' disconnected')));
-            // send_message($response);
-        }
+    if (time() - $last_activity > 30) {
+        break;
     }
 }
 
 socket_close($socket);
+exit(0);
